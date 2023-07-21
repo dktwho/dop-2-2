00708af5eabc627ab7c667031041365778100c36
Alexander Korobov <dktwho@mail.ru>
2023-07-21 19:01:07 +0300

    add style and logic isActive

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -6,15 +6,20 @@ import {PageThree} from "./pages/PageThree";
 import {Error404} from "./pages/Error404";
 import {Navigate, NavLink, Route, Routes} from "react-router-dom";
 
+
 export const Site = () => {
     return (
         <div>
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    <div><NavLink to={'/PageOne'}>Page 1</NavLink></div>
-                    <div><NavLink to={'/PageTwo'}>Page 2</NavLink></div>
-                    <div><NavLink to={'/PageThree'}>Page 3</NavLink></div>
+                    <div><NavLink to={'/PageOne'} className={({isActive}) => isActive ? styles.active : styles.navLink}>Page
+                        1</NavLink></div>
+                    <div><NavLink to={'/PageTwo'} className={({isActive}) => isActive ? styles.active : styles.navLink}>Page
+                        2</NavLink></div>
+                    <div><NavLink to={'/PageThree'}
+                                  className={({isActive}) => isActive ? styles.active : styles.navLink}>Page 3</NavLink>
+                    </div>
                 </div>
                 <div className={styles.content}>
                     <Routes>
