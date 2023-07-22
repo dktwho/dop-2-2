bace64f9e90cab029588a153f2d9fcfa26e7e48a
Alexander Korobov <dktwho@mail.ru>
2023-07-22 20:15:24 +0300

    prepared fo conditional  rendering checked id to isNaN if id will be only number

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import {PagesType} from "../../dataState/dataState";
 import {useParams} from "react-router-dom";
+import {Error404} from "./Error404";
 
 type PropsType = {
     pages: Array<PagesType>
 }
-export const Page = (props:PropsType ) => {
+export const Page = (props: PropsType) => {
     const params = useParams()
     let element = Number(params.id)
 
-
-
-
-
     return (
         <div>
-
-            <div>{props.pages[element].heading}</div>
-            <div>{props.pages[element].about}</div>
+            {isNaN(element)
+                ? <Error404/>
+                : <>
+                    <div>{props.pages[element].heading}</div>
+                    <div>{props.pages[element].about}</div>
+                </>}
         </div>
     );
 };
